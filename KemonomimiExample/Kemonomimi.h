c91d91838c225b69c33237de7c736fe4404728d5
MrHypercube <xarkingaming@gmail.com>
2017-11-05 12:59:43 -0800

    Added new features to the test.

--- KemonomimiExample/Kemonomimi.h
+++ KemonomimiExample/Kemonomimi.h
@@ -3,14 +3,16 @@
 class Kemonomimi
 {
 public:
-	//The values for the kenomimi
+	// The values for the kenomimi
 	char* Name;
 	char* Type;
 	char* Gender;
 	float Weight;
-	int Patted = 0;
+	bool LikesYou = false;
+	int Patted;
+	int Points;
 
-	//Prints the following value
+	// Prints the following value
 
 	void LogName()
 	{
@@ -35,30 +37,59 @@ public:
 		std::cout << Weight << "\n";
 	}
 
+	void LogLikes()
+	{
+		std::cout << "Likes you: " << LikesYou << "\n";
+	}
+
 
 	void LogAll()
 	{
 		std::cout << Type << "\n";
 		std::cout << Gender << "\n";
 		std::cout << Weight << "\n";
+		std::cout << "Likes you: " << LikesYou << "\n";
 	}
 
-	//A fun function
+	// A fun function
 
 	void Headpat()
 	{
-		Patted = Patted + 1;
+		if (Patted <= 2)
+		{
+			Points = Points + 1;
+		}
+
+
+		if (Patted > 2)
+		{
+			Points = Points - 1;
+			std::cout << Name << " Is starting to get kind of annoyed.";
+		}
+
+
 		if (Type == "Fox" || Type == "fox" || Type == "Kitsune" || Type == "kitsune" || Type == "Bunny" || Type == "bunny" || Type == "Rabbit" || Type == "rabbit")
 		{
-			std::cout << Name << " Is really loving you right now! \n";
+
+			if (Patted < 2)
+			{
+				std::cout << Name << " Is really loving you right now! \n";
+			}
+
 		}
+
+
 		if (Type == "Cat" || Type == "cat" || Type == "Neko" || Type == "neko")
 		{
 			std::cout << Name << " has started purring! \n";
 		}
+
+
 		if (Type == "Dog" || Type == "dog" || Type == "Wolf" || Type == "wolf" || Type == "Okami" || Type == "okami")
 		{
 			std::cout << Name << " has started panting in happyness! \n";
 		}
+
+
 	}
 };
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,23 +1,69 @@
-// Test.cpp : Defines the entry point for the console application.
-//
+// Import Stuff
+
 #include "stdafx.h";
 #include<iostream>
 #include<Kemonomimi.h>
+#include <string>
 
+// The main function of the app
 
 int main()
-{
-	bool x;
+{ 
+	// Main stuff
+
+	std::string x;
+
 	Kemonomimi* Test = new Kemonomimi();
+
+	// Specify values
+
 	Test->Name = "Kitsune";
 	Test->Type = "Fox";
-	Test->Gender = "Male";
+	Test->Gender = "Female";
 	Test->Weight = 200.00f;
+
+	// Log everything.
+
 	Test->LogAll();
-	Test->Headpat();
-	std::cout << Test->Patted;
-	
-	std::cin >> x;
+	std::cout << "\n";
+
+	// Get input
+	std::cout << "\n Would you like to headpat " << Test->Name << "?" << "\n";
+	std::cout << "\n";
+	std::cout << "\n";
+	std::cout <<  "A: Yes \n";
+	std::cout << "\n";
+	std::cout << "B: Yes \n";
+	std::cout << "\n";
+	std::cout << "P: Get the ammount of times patted \n";
+	std::cout << "\n";
+
+		start:	
+
+		std::cin >> x;
+
+		if (x == "A" || x == "a")
+		{
+			Test->Headpat();
+			goto start;
+			
+		}
+
+		if (x == "B" || x == "b")
+		{
+			std::cout << "Ok then I guess. \n";
+			goto start;
+		}
+
+		if (x == "P" || x == "p")
+		{
+			std::cout << Test->Patted << "\n";
+			goto start;
+		}
+
+
+
+	// End the app
     return 0;
 }
 
